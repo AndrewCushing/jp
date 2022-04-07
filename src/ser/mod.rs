11b7162e2247c54bea17796efe7696a4417acc51
Andrew Cushing <Andrew.Cushing@nationwide.co.uk>
2022-04-07 22:29:24 +0100

    Tidied

--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -1,17 +1,9 @@
 use protobuf::SingularPtrField;
 use crate::schemas::structs::payment_envelope::Envelope;
-use crate::schemas::structs::payment_envelope_header::{Header, Properties};
-use super::schemas::structs::payment_envelope;
+use crate::schemas::structs::payment_envelope_header::Header;
 
-pub fn serialise(envelope: Envelope) -> Vec<u8> {
-    let mut envelope1: Envelope = payment_envelope::Envelope {
-        version: "".to_string(),
-        header: Default::default(),
-        body: Default::default(),
-        unknown_fields: Default::default(),
-        cached_size: Default::default()
-    };
-    envelope1.header = SingularPtrField::from(Option::Some(Header{
+pub fn serialise(envelope: &mut Envelope) -> Vec<u8> {
+    envelope.header = SingularPtrField::from(Option::Some(Header{
         properties: Default::default(),
         signatures: Default::default(),
         state: Default::default(),
